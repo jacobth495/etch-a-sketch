92935f1b5e093e99e0986d469d3c1fc8e4e95b5f
Jacob Henderson <jacobth495@gmail.com>
2022-08-30 16:03:36 -0500

    Changed pixel class to pix Added the ids square to container divs Added mouseover event for squares

--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const container = document.querySelector('#container');
 
 for (i = 1; i <= 16; i++) {
   div = document.createElement('div');
-  div.classList.add('pixel');
+  div.id = 'square'
   container.appendChild(div);
+};
+
+
+const pixels = document.querySelectorAll('#square')
+
+for (let pixel of pixels) {
+  pixel.addEventListener('mouseover', () => {
+    pixel.classList.toggle("pix")
+  });
 }
+
 
 //give div class name
 //add an event listener to the divs
